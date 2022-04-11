4228ec69a459d180f392c51b2213f5b6667cf8d0
WoojaeJang <staphano77x@naver.com>
2022-04-11 12:04:36 +0900

    1주차 3번문제 풀이 업로드

---
+++ code/practice/w01_prc_3_다리를지나는트럭.py
@@ -0,0 +1,39 @@
+def solution(bridge_length, weight, truck_weights):   
+    
+    answer = 0       
+    
+    on_bridge_truck = []
+    time_list = []
+    
+    for idx in range(len(truck_weights)) :       
+        
+        if len(time_list) > 0 :
+            time_list = list(map(lambda x:x - 1, time_list))
+            if time_list[0] == 0 :
+                time_list.pop(0)
+                on_bridge_truck.pop(0)
+            
+        if sum(on_bridge_truck) + truck_weights[idx] > weight :    
+            while sum(on_bridge_truck) + truck_weights[idx] > weight :
+                del_time = time_list.pop(0)
+                on_bridge_truck.pop(0)
+                time_list = list(map(lambda x:x - del_time, time_list))
+                answer += del_time
+            
+        on_bridge_truck.append(truck_weights[idx])
+        time_list.append(bridge_length)
+        answer += 1     
+    
+    answer += time_list[-1]
+    
+    return answer
+'''
+나름 잘 푼듯??
+'''
+
+if __name__ == "__main__" :
+    bridge_length = 2
+    weight = 10
+    truck_weights = [7, 4, 5, 6]
+    
+    print(solution(bridge_length, weight, truck_weights))
